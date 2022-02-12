de45e5c3f85ab74910a282934505202e12beca6a
Aaroneavecdeuxaa <gaarone.ag18@gmail.com>
2022-02-12 16:39:03 +0000

    <votre message commit>

--- main/01-facile.js
+++ main/01-facile.js
@@ -23,5 +23,19 @@ console.log(signe(0))
  * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
  */
 function nombresPairs(nombres) {
+    let TableauPaire = [ ];
+    let Tab = 0;
+let nombre = 0;
+while (nombre < nombres.length) {
+if (nombres[nombre] % 2 === 0){
+    TableauPaire[Tab] = nombres[nombre];
+    Tab+++;
 
 }
+nombre+++
+}
+    return TableauPaire
+}
+
+let MonTableau = [1, 2 ,3 , 4 ,5 ,6 , 7, 8,9, 10];
+console.log(nombresPairs(MonTableau))
